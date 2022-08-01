94313e3aec089d02bfa63d66ddfb436d604060a0
Oussama-hamdi <86133676+Oussama-hamdi@users.noreply.github.com>
2022-08-01 15:12:29 +0100

    Added Settings Container

--- js/master.js
+++ js/master.js
@@ -1,3 +1,31 @@
+// Toggle Spin Class on Icon
+document.querySelector(".toggle-settings .icon").onclick = function () {
+  // toggle Class fa-spin
+  this.classList.toggle("fa-spin");
+
+  // Toggle Class Show
+  document.querySelector(".settings-box").classList.toggle("show");
+};
+
+// Switch Colors
+const colorsLi = document.querySelectorAll(".colors-list li");
+
+colorsLi.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    colorsLi.forEach((li) => {
+      li.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    // Set Color On Root
+    document.documentElement.style.setProperty(
+      "--main-color",
+      e.target.dataset.color
+    );
+  });
+});
+
+// Select Landing Page Elements
 let images = [
   "images/01.jpeg",
   "images/02.jpeg",
@@ -17,3 +45,5 @@ setInterval(() => {
 function random(arr) {
   return Math.floor(Math.random() * arr.length);
 }
+
+// Move To Video 9

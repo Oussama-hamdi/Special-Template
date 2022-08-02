d9be0e4110ad194031ddbf8af15f57341027b666
Oussama-hamdi <86133676+Oussama-hamdi@users.noreply.github.com>
2022-08-02 18:01:02 +0100

    Added New Updates on Option-box

--- js/master.js
+++ js/master.js
@@ -1,3 +1,21 @@
+// Check If Local Storage have color-option Value
+let mainColor = localStorage.getItem("color-option");
+
+if (mainColor !== null) {
+  // Add Color To css root
+  document.documentElement.style.setProperty("--main-color", mainColor);
+
+  document.querySelectorAll(".colors-list li").forEach((e) => {
+    e.classList.remove("active");
+    if (e.dataset.color === mainColor) {
+      e.classList.add("active");
+    }
+  });
+  // document
+  //   .querySelector(`[data-color="${localStorage.getItem("color-option")}"]`)
+  //   .classList.add("active");
+}
+
 // Toggle Spin Class on Icon
 document.querySelector(".toggle-settings .icon").onclick = function () {
   // toggle Class fa-spin
@@ -12,9 +30,11 @@ const colorsLi = document.querySelectorAll(".colors-list li");
 
 colorsLi.forEach((li) => {
   li.addEventListener("click", (e) => {
+    // Remove Active Class From All li's
     colorsLi.forEach((li) => {
       li.classList.remove("active");
     });
+    // Add Class Active To The Target li
     e.target.classList.add("active");
 
     // Set Color On Root
@@ -22,6 +42,8 @@ colorsLi.forEach((li) => {
       "--main-color",
       e.target.dataset.color
     );
+    // Add Color To Local Storage
+    localStorage.setItem("color-option", e.target.dataset.color);
   });
 });
 
@@ -46,4 +68,4 @@ function random(arr) {
   return Math.floor(Math.random() * arr.length);
 }
 
-// Move To Video 9
+// Move To Video 11
